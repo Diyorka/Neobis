24b13424291382972b090e483e00c1803a17547a
Diyor <diyorp2@mail.ru>
2022-10-06 13:56:29 +0600

    worker commit

---
+++ week3/src/part2/Worker.java
@@ -0,0 +1,71 @@
+package part2;
+
+public class Worker {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String position;
+
+    public Worker() {
+    }
+
+    public Worker(int id, String firstName, String lastName, String phoneNumber, String position) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+}
