0970b98920ec69f522931631cf71e9e5245c28e2
Diyor Umurzakov <89022565+Diyorka@users.noreply.github.com>
2022-11-20 13:04:28 +0600

    Delete CourseProject3 directory

--- CourseProject3/src/main/java/com/example/courseproject3/repositories/OrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.courseproject3.repositories;
-
-import com.example.courseproject3.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrdersRepository extends JpaRepository<Order, Integer> {
-}
--- CourseProject3/src/main/java/com/example/courseproject3/util/NotAddedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseproject3.util;
-
-public class NotAddedException extends RuntimeException{
-    public NotAddedException(String msg){
-        super(msg);
-    }
-}
